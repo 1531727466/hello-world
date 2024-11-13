e555121a940b09e2f1a2ee396752b235b8d7d6d6
chenchunyu <1531727466@qq.com>
2024-11-13 15:37:37 +0800

    更改

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,6 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
 import Image from "next/image";
-import { sql } from "@vercel/postgres";
-import { JSX } from "react";
-
-export async function Cart({
-  params,
-}: {
-  params: { user: string };
-}): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
-
-  return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
-    </div>
-  );
-}
 
 export default function Page() {
   return (
